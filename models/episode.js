415bf391aad3df9f518753c9ac50f0fed1ec825b
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-04-15 10:28:01 -0400

    episode model

---
+++ models/episode.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Episode extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Episode.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    lengh: DataTypes.INTEGER,
+    photo: DataTypes.STRING,
+    animeId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Episode',
+  });
+  return Episode;
+};
