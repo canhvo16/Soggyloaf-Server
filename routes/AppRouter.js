e9b06c89e0b044f066b2e97c9c4832ead65f9064
Guillermo Palencia <guillermopalencia@yahoo.com>
2022-04-15 14:08:21 -0400

    changes

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,11 +1,11 @@
 const Router = require('express').Router()
 const AnimeRouter = require('./AnimeRouter')
-const EpisdoeRouter = require('./EpisodeRouter')
+const EpisodeRouter = require('./EpisodeRouter')
 const UserRouter = require('./UserRouter')
 const WatchListRouter = require('./WatchListRouter')
 
 Router.use('/anime', AnimeRouter)
-// Router.use('/episodes', EpisdoeRouter)
+Router.use('/episodes', EpisodeRouter)
 // Router.use('/watchlist', WatchListRouter)
 Router.use('/users', UserRouter)
 
--- routes/EpisodeRouter.js
+++ routes/EpisodeRouter.js
@@ -1,1 +1,8 @@
 const Router = require('express').Router()
+const controller = require('../controllers/EpisodeController')
+
+Router.get('/', controller.GetEpisodes)
+
+Router.post('/:anime_id', controller.CreateEpisode)
+
+module.exports = Router
